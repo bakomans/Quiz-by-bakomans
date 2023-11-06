0ee3bffa76535bfdd8db3d21840074a89665238e
bakomans <bakomans@gmail.com>
2023-11-06 08:09:06 +0000

    add a question to questions.js file

---
+++ starter/assets/js/question.js
@@ -0,0 +1,28 @@
+// creating a question 
+
+var questions = [
+    {
+        question: "What Pierogi are?",
+        choices: ["Super tasty dumplings from Poland", "Weirdo kitchen pan", "is a different name for Sunday roast in Wales"],
+        correctAnswer: 0
+    },
+    {
+        question: "Which country are you visiting if you are in the Taj Mahal",
+        choices: ["pakistan", "India", "France", "USA", "Cambodia"],
+        correctAnswer: 1
+    },
+    {
+        question: "What was the name of the crime boss who was head of the feared Chicago Outfit",
+        choices: ["Don Corleone", "Al Capone", "Al Bundy", "Alf"],
+        correctAnswer: 1
+    },
+    {
+        question: "What does a sommelier specialize in?",
+        choices: ["Creating web pages","Beer", "Wine", "Vodka"],
+        correctAnswer: 2
+    },
+    
+];
+
+
+export default questions;
